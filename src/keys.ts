ba6e588706b1d157d32856aa3d01913de615d319
Thomas Hickman <Thomas.Hickman42@gmail.com>
2016-03-28 22:01:53 +0100

    Updated keys.ts

--- src/keys.ts
+++ src/keys.ts
@@ -1,8 +1,10 @@
 export import Arrangement = require("./Keys/Arrangement")
 export import FixedArrangement = require("./Keys/FixedArrangement")
 export import Integer = require("./Keys/Integer")
-export import NonUniqueArray = require("./Keys/NonUniqueArray")
+export import NonUniqueArrayFromKey = require("./Keys/NonUniqueArrayFromKey")
+export import NonUniqueFixedLengthArray = require("./Keys/NonUniqueFixedLengthArray")
 export import NumericArrangement = require("./Keys/NumericArrangement")
+export import MappedKey = require("./Keys/MappedKey")
 
 export import util = require("./Keys/keysUtils")
 
