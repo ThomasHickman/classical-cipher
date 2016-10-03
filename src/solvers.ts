87e69be90ab8bfabe6a0ada19c94448f8b97f45d
Thomas Hickman <Thomas.Hickman42@gmail.com>
2016-10-03 20:44:07 +0100

    Removed Playfair and SimmulatedAnnealing from master

--- src/solvers.ts
+++ src/solvers.ts
@@ -2,8 +2,6 @@ export {Solver} from "./Solvers/Solver";
 
 import BruteForce from "./Solvers/BruteForce"
 import HillClimbing from "./Solvers/HillClimbing"
-import SimmulatedAnnealing = require("./Solvers/SimmulatedAnnealing");
 
 export var bruteForce = new BruteForce();
 export var hillClimbing = new HillClimbing();
-export var simmulatedAnnealing = new SimmulatedAnnealing();
